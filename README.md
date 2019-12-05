e7ca48ad1e767473cbea4b0ce7d6ef10162487f4
Baptiste <baptiste.diot@hotmail.fr>
2019-12-05 17:33:57 +0100

    Did birthday greeter

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # m1-isc-2019
+
+Run the "Main" application.
+
+The path for the "humans.txt" file is in the "Main" file in case something's wrong.
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static final String FILE_LIST_EMPLOYEES = "./resources/humans.txt";
+
+    public static void main(String[] args) throws IOException, ParseException {
+        ArrayList<String> getAllEmployees = ListEmployeesFileReader.getListEmployees(FILE_LIST_EMPLOYEES);
+
+        BirthdayGreeter.greetEmployeesBirthday(getAllEmployees);
+    }
+}
